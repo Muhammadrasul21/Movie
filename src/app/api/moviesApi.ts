a0abe6e42248b35ae69e91c86ec13f9f95877c64
Muhammadrasul Gulomjonov <theboy6878@gmail.com>
2025-09-05 15:22:33 +0500

    added trailer in detail page

--- src/app/api/moviesApi.ts
+++ src/app/api/moviesApi.ts
@@ -18,3 +18,9 @@ export const getGenres = () => apiFetcher("/genre/movie/list");
 // Qidiruv
 export const getSearch = (params: { query: string }) =>
   apiFetcher("/search/movie", params);
+
+// trailers (videos)
+export const getMovieVideos = (id: number) => getSingleItems(id, "videos");
+
+// casts (credits)
+export const getMovieCredits = (id: number) => getSingleItems(id, "credits");
